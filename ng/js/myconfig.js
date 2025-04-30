2ed605b82669a9f8e5fb9e34b16635ddd1d3dd45
hypnoticnightmare <mikumizuki97@gmail.com>
2025-04-30 14:53:50 +0200

    Update myconfig.js

--- ng/js/myconfig.js
+++ ng/js/myconfig.js
@@ -1,9 +1,9 @@
 var sucrette = {
     "avatar":{
         "skin":"lys",
-        "hair":"pink",
+        "hair":"orange",
         "eyebrows":"1-5b720362ccbfc015",
-        "eyesColor":"green",
+        "eyesColor":"purple",
         "eyes":"1-c8f85ec8f0f3ab2d",
         "mouth":"2-d52f3502c5814f22",
         "expressionPreset":7,
