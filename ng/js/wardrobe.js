fa3b3a8da0db98952ab79e05d7b643190f42c1b9
hypnoticnightmare <mikumizuki97@gmail.com>
2026-09-02 02:48:31 +0200

    Update wardrobe.js

--- ng/js/wardrobe.js
+++ ng/js/wardrobe.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
         $.get("./data/avatar.json", dbAvatar => {
             $.get("./data/room.json", dbRoom => {
                 $.get("./data/pet.json", dbPet => {
-                    $.get("./data/crush.json", bCrush => {
-                
+                    $.get("./data/crush.json", dbCrush => {
+                    
                         cloth = dbCloth;
                         avatar = dbAvatar;
                         room = dbRoom;
